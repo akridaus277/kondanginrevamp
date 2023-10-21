04ac620fe7c6e5b03b0316c6c6dfd3d75b9128f5
akri <fachryfirdaus277@yahoo.com>
2023-10-21 23:28:33 +0700

    testing clear cache

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -4,7 +4,7 @@ import  ReactDOM  from 'react-dom';
 function Example() {
     return (
         <div className="example">
-            This is a React component!
+            This is a React component! Hiya hiya tanpa clear cache
         </div>
     );
 }
